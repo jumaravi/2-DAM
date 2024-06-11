a82789200a9ed3cba306edfe8d849d565577f574
jumaravi <123063070+jumaravi@users.noreply.github.com>
2024-06-11 10:40:19 +0200

    cambios front
    Front funciona completo

--- rstudio/MetalDasboard/app.R
+++ rstudio/MetalDasboard/app.R
@@ -24,9 +24,8 @@ calculateTimeLeft <- function() {
 }
 
 # Temporizador reactivo para actualizar cada 10 minutos en los minutos 00, 10, 20, 30, 40, 50
-autoUpdate <- reactiveTimer({
-  calculateTimeLeft()
-})
+#autoUpdate <- reactiveTimer(calculateTimeLeft() * 1000)
+autoUpdate <- reactiveTimer(10000)
 
 # Define UI for application
 ui <- fluidPage(
@@ -63,7 +62,7 @@ ui <- fluidPage(
 # Define server logic required to draw the plot
 server <- function(input, output, session) {
   
-  # Reactive value to store the selected interval and currency
+  # Reactive value to store the selected interval
   values <- reactiveValues(interval = "h1")
   
   # Function to fetch data from API
@@ -99,20 +98,36 @@ server <- function(input, output, session) {
       return(NULL)
     } else {
       data <- fromJSON(content(response, "text", encoding = "UTF-8"))
-      df <- data.frame(
-        datetime = as.POSIXct(data$datetime, format="%Y-%m-%dT%H:%M:%OSZ", tz="UTC"),
-        openPrice = data$openPrice,
-        closePrice = data$closePrice,
-        highPrice = data$highPrice,
-        lowPrice = data$lowPrice
-      )
+      if (length(data) == 0) {
+        
+        if (!(interval %in% c("D", "S"))){
+          # Eliminating duplicates
+          showNotification("No hay datos disponibles", type = "warning")
+        }
+        
+       df <- data.frame(
+          datetime = as.POSIXct(character()),
+          openPrice = numeric(),
+          closePrice = numeric(),
+          highPrice = numeric(),
+          lowPrice = numeric()
+        )
+      } else {
+        df <- data.frame(
+          datetime = as.POSIXct(data$datetime, format="%Y-%m-%dT%H:%M:%OSZ", tz="UTC"),
+          openPrice = data$openPrice,
+          closePrice = data$closePrice,
+          highPrice = data$highPrice,
+          lowPrice = data$lowPrice
+        )
+      }
     }
+    
     
     if (interval %in% c("D", "S")){
       # Eliminating duplicates
       df_hist <- distinct(df_hist, datetime, .keep_all = TRUE)
     }
-    
     
     # Combine historical and current data
     if (!is.null(df_hist)) {
@@ -123,47 +138,71 @@ server <- function(input, output, session) {
     return(df_seg)
   }
   
-  
-  # Function to update the plot
-  updatePlot <- reactive({
-    autoUpdate() # Esto activará el temporizador y actualizará cada 10 minutos en los minutos 00, 10, 20, 30, 40, 50
+  # Function to create the plot
+  createPlot <- function(df_seg, metal, interval) {
+    if (is.null(df_seg) || nrow(df_seg) == 0) {
+      df_seg <- data.frame(datetime = as.POSIXct(character()), 
+                           openPrice = numeric(), 
+                           closePrice = numeric(), 
+                           highPrice = numeric(), 
+                           lowPrice = numeric())
+    }
     
+    plot_ly(data = df_seg, x = ~datetime, type = 'candlestick',
+            open = ~openPrice, close = ~closePrice,
+            high = ~highPrice, low = ~lowPrice) %>%
+      layout(title = paste(metal, "Price (", toupper(interval), ")", sep = " "),
+             xaxis = list(rangeslider = list(visible = FALSE))) %>%
+        plotly::config(scrollZoom = TRUE)
+    
+  }
+  
+  
+  # Function to update the plot data
+  updatePlotData <- function(proxy, df_seg) {
+    plotlyProxyInvoke(proxy, "restyle", list(
+      x = list(df_seg$datetime),
+      open = list(df_seg$openPrice),
+      close = list(df_seg$closePrice),
+      high = list(df_seg$highPrice),
+      low = list(df_seg$lowPrice)
+    ))
+  }
+  
+  # Render the plot
+  output$distPlot <- renderPlotly({
     metal <- input$metal_select
     interval <- values$interval
     df_seg <- fetchData(metal, interval)
     req(df_seg)
-    p <- plot_ly(data = df_seg, x = ~datetime, type = 'candlestick',
-                 open = ~openPrice, close = ~closePrice,
-                 high = ~highPrice, low = ~lowPrice) %>%
-      layout(title = paste(metal, "Price (", toupper(interval), ")", sep = " "),
-             xaxis = list(rangeslider = list(visible = FALSE)))
-    p <- plotly::config(p, scrollZoom = TRUE)
-    p
-    
+    createPlot(df_seg, metal, interval)
   })
   
-  # Observers to update the interval based on button clicks
-  observeEvent(input$H1_button, {
-    values$interval <- "h1"
+  
+  # Observer to update the interval based on button clicks
+  observeEvent(input$H1_button, { values$interval <- "h1" })
+  observeEvent(input$H4_button, { values$interval <- "h4" })
+  observeEvent(input$D_button, { values$interval <- "D" })
+  observeEvent(input$S_button, { values$interval <- "S" })
+  
+  # Observer to update the plot data when the interval or metal selection changes
+  observe({
+    metal <- input$metal_select
+    interval <- values$interval
+    df_seg <- fetchData(metal, interval)
+    req(df_seg)
+    plotlyProxy("distPlot", session) %>% updatePlotData(df_seg)
   })
   
-  observeEvent(input$H4_button, {
-    values$interval <- "h4"
+  # Observer to update the plot data periodically
+  observe({
+    autoUpdate()
+    metal <- isolate(input$metal_select)
+    interval <- isolate(values$interval)
+    df_seg <- fetchData(metal, interval)
+    req(df_seg)
+    plotlyProxy("distPlot", session) %>% updatePlotData(df_seg)
   })
-  
-  observeEvent(input$D_button, {
-    values$interval <- "D"
-  })
-  
-  observeEvent(input$S_button, {
-    values$interval <- "S"
-  })
-  
-  # Render the plot
-  output$distPlot <- renderPlotly({
-    updatePlot()
-  })
-  
 }
 
 # Run the application 
